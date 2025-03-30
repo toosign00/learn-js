4b07b830292387bffc5c5a3d6e46bfe54d1ad8e1
toosign <kevinsoo1014@gmail.com>
2025-03-31 01:07:23 +0900

    📚 Learn :  JavaScript 첫 수업 - 인라인 JavaScript - 외부 JavaScript - 명령문과 주석 - 변수 - 리터럴 - 데이터 타입 - 연산자
    이슈 없음

--- workspace/ch01/ex01-05.js
+++ workspace/ch01/ex01-05.js
@@ -2,3 +2,19 @@
 변수 선언
 */
 
+var a;
+var b, c;
+var d = 10;
+var e = 100,
+  f = 200;
+
+var user = '노현수';
+var age = 20;
+
+var message = '노현수가 "안녕하세요."라고 말했습니다.';
+var message = '노현수가 \"피곤해"\ 라고 말했습니다.' // 이스케이프 문자(\) 사용
+var message = '노현수가 \'피곤해\'라고 말했습니다.' // 이스케이프 문자(\) 사용
+
+
+
+console.log(user, message);
--- workspace/ch01/ex01-06.js
+++ workspace/ch01/ex01-06.js
@@ -2,3 +2,25 @@
 기본 데이터 타입 - Number, String, Boolean, undefined, null
 */
 
+// number
+
+let num = 100;
+let floatNum = 3.14; // 부동소수점 숫자
+
+// string
+let s1 = 'single quote';
+let s2 = 'double quote';
+
+// boolean
+
+let b1 = true;
+let b2 = false;
+
+// undefined
+let emptyVal;
+
+// null
+let nullVal = null;
+
+console.log(num, floatNum, s1, s2, b1, b2, emptyVal, nullVal);
+console.log(typeof num, typeof floatNum, typeof s1, typeof s2, typeof b1, typeof b2, typeof emptyVal, typeof nullVal);
