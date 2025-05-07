8e902404d6af2da0239e48b7c931a36a0ea47790
toosign <kevinsoo1014@gmail.com>
2025-05-07 13:04:13 +0900

    Merge

--- workspace/ch07/ex07-06.js
+++ workspace/ch07/ex07-06.js
@@ -7,7 +7,10 @@
     console.log(Object.values(haru)); // ['하루', 5]
     console.log(Object.entries(haru)); // [['name', '하루'], ['age', 5]]
     // fromEntries
-    const newUser = Object.fromEntries([['name', '나무'], ['age', 8]]);
+    const newUser = Object.fromEntries([
+        ['name', '나무'],
+        ['age', 8],
+    ]);
     console.log(newUser); // { name: '나무', age: 8 }
     const haru2 = haru;
     haru.age++;
--- workspace/ch07/ex07-06.ts
+++ workspace/ch07/ex07-06.ts
@@ -1,7 +1,6 @@
 // 내장 객체 - Object
 
-(()=>{
-  
+(() => {
   const haru = { name: '하루', age: 5 };
 
   // keys, values, entries
@@ -10,7 +9,10 @@
   console.log(Object.entries(haru)); // [['name', '하루'], ['age', 5]]
 
   // fromEntries
-  const newUser = Object.fromEntries([['name', '나무'], ['age', 8]]);
+  const newUser = Object.fromEntries([
+    ['name', '나무'],
+    ['age', 8],
+  ]);
   console.log(newUser); // { name: '나무', age: 8 }
 
   const haru2 = haru;
@@ -21,6 +23,4 @@
   const haru3 = Object.assign({}, haru);
   haru.age++;
   console.log(haru.age, haru3.age); // 7 6
-
 })();
-
